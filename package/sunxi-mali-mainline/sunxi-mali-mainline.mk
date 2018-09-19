8d7bd92ffa5fc6bd5f46c6960d2310b258cd9deb
Paul DeMarco <paulmdemarco@gmail.com>
2018-09-18 20:20:28 -0400

    Allows for selection of fbdev/X11/Wayland blobs and header files.

--- package/sunxi-mali-mainline/sunxi-mali-mainline.mk
+++ package/sunxi-mali-mainline/sunxi-mali-mainline.mk
@@ -14,6 +14,8 @@ SUNXI_MALI_MAINLINE_EULA_NO_SPACES = EULA_for_Mali_400MP_AW.pdf
 SUNXI_MALI_MAINLINE_LICENSE_FILES = $(SUNXI_MALI_MAINLINE_EULA_NO_SPACES)
 
 SUNXI_MALI_MAINLINE_REV = $(call qstrip,$(BR2_PACKAGE_SUNXI_MALI_MAINLINE_REVISION))
+SUNXI_MALI_MAINLINE_BLOB_TYPE = $(call qstrip,$(BR2_PACKAGE_SUNXI_MALI_MAINLINE_BLOB_TYPE))
+
 
 ifeq ($(BR2_arm),y)
 SUNXI_MALI_MAINLINE_ARCH=arm
@@ -24,9 +26,9 @@ endif
 define SUNXI_MALI_MAINLINE_INSTALL_STAGING_CMDS
 	mkdir -p $(STAGING_DIR)/usr/lib $(STAGING_DIR)/usr/include
 
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/fbdev/*.so* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/$(SUNXI_MALI_MAINLINE_BLOB_TYPE)/*.so* \
 		$(STAGING_DIR)/usr/lib/
-	cp -rf $(@D)/include/fbdev/* $(STAGING_DIR)/usr/include/
+	cp -rf $(@D)/include/$(SUNXI_MALI_MAINLINE_BLOB_TYPE)/* $(STAGING_DIR)/usr/include/
 
 	$(INSTALL) -D -m 0644 package/sunxi-mali-mainline/egl.pc \
 		$(STAGING_DIR)/usr/lib/pkgconfig/egl.pc
@@ -36,7 +38,7 @@ endef
 
 define SUNXI_MALI_MAINLINE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib
-	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/fbdev/*.so* \
+	cp -rf $(@D)/$(SUNXI_MALI_MAINLINE_REV)/$(SUNXI_MALI_MAINLINE_ARCH)/$(SUNXI_MALI_MAINLINE_BLOB_TYPE)/*.so* \
 		$(TARGET_DIR)/usr/lib/
 endef
 
